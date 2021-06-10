7ce10c87b4d854dc88abfe2d1ae2ccbc937d5a15
Yuhei Yasuda <yuhei.yasuda1003@gmail.com>
2021-06-10 09:33:03 +0900

    fix comment

--- packages/eleventy-typescript/src/filters.ts
+++ packages/eleventy-typescript/src/filters.ts
@@ -1,3 +1,7 @@
+// Eleventy Provided Universal Filters
+// https://www.11ty.dev/docs/filters/#eleventy-provided-universal-filters
+// https://github.com/11ty/eleventy/blob/master/src/defaultConfig.js
+
 import { EleventyTemplate, EleventyTemplateData } from "@11ty/eleventy";
 import getCollectionItemFilter from "@11ty/eleventy/src/Filters/GetCollectionItem";
 import slugFilter from "@11ty/eleventy/src/Filters/Slug";
@@ -5,10 +9,6 @@ import { EleventyTypeScriptConfig } from "./config";
 import urlFilter from "./url-filter";
 
 export const EleventyFilters = {
-	// Eleventy Provided Universal Filters
-	// https://www.11ty.dev/docs/filters/#eleventy-provided-universal-filters
-	// https://github.com/11ty/eleventy/blob/master/src/defaultConfig.js
-
 	url: (url: string) => urlFilter(url, EleventyTypeScriptConfig.pathPrefix),
 
 	slug: slugFilter,
